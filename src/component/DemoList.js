54f8cd9ecb8f47c82b6b639b40fd1ee8ea8099ca
Yang <269977322@qq.com>
2017-06-05 20:49:18 +0800

    temp

---
+++ src/component/DemoList.js
@@ -0,0 +1,73 @@
+import React, {Component} from 'react';
+import {View, Text, ListView, TouchableOpacity, Image, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        padding: 10,
+        backgroundColor: '#F6F6F6',
+    },
+    thumb: {
+        width: 50,
+        height: 50,
+    },
+});
+
+/**
+ * ListView
+ */
+class DemoList extends Component {
+
+    constructor(props) {
+        super(props);
+        // 初始化模拟数据
+        const dataSource = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+
+        this.state = {
+            dataSource: dataSource.cloneWithRows(props.items)
+        };
+    }
+
+    renderRow = (item, i) => {
+
+        let pic = {
+            uri: "https://i1.hoopchina.com.cn/blogfile/201705/18/BbsImg149509482112629_614x887.jpg?x-oss-process=image/resize,w_800/format,webp"
+        };
+
+        return (
+            <TouchableOpacity>
+                <View style={styles.row}>
+                    <Image
+                        source={pic} style={styles.thumb}/>
+                    <View style={{flexDirection: 'column'}}>
+                        <Text style={{flex: 1, fontSize: 16, color: 'blue'}}>
+                            {item.name}
+                        </Text>
+                        <Text>
+                            {item.desc}
+                        </Text>
+                    </View>
+
+                </View>
+            </TouchableOpacity>
+        );
+    }
+
+    render() {
+        return (
+            <View style={{flex: 1}}>
+                <ListView dataSource={this.state.dataSource}
+                          renderRow={this.renderRow}/>
+            </View>
+        );
+    }
+}
+
+DemoList.propTypes = {
+    items: PropTypes.array.isRequired,
+};
+
+export default DemoList;
--- src/screen/MainScreen.js
+++ src/screen/MainScreen.js
@@ -1,87 +1,6 @@
 import React, {Component} from 'react';
 import {View, Text, ListView, ScrollView, Button, TouchableOpacity, Image, StyleSheet} from 'react-native';
-
-const styles = StyleSheet.create({
-    row: {
-        flexDirection: 'row',
-        justifyContent: 'flex-start',
-        padding: 10,
-        backgroundColor: '#F6F6F6',
-    },
-    thumb: {
-        width: 50,
-        height: 50,
-    },
-});
-
-/**
- * ListView
- */
-class List extends Component {
-
-    constructor(props) {
-        super(props);
-        // 初始化模拟数据
-        const dataSource = new ListView.DataSource({
-            rowHasChanged: (r1, r2) => r1 !== r2
-        });
-
-        this.state = {
-            dataSource: dataSource.cloneWithRows([
-                {
-                    name: '陶宏飞',
-                    desc: '无力滔滔'
-                }, {
-                    name: '周志忠',
-                    desc: '123'
-                }, {
-                    name: '舒韵',
-                    desc: '3214'
-                }, {
-                    name: '宋肖俊',
-                    desc: '5234'
-                }, {
-                    name: '杨念一',
-                    desc: '3568'
-                }
-            ])
-        };
-    }
-
-    renderRow = (item, index) => {
-
-        let pic = {
-            uri: "https://i1.hoopchina.com.cn/blogfile/201705/18/BbsImg149509482112629_614x887.jpg?x-oss-process=image/resize,w_800/format,webp"
-        };
-
-        return (
-            <TouchableOpacity>
-                <View style={styles.row}>
-                    <Image
-                        source={pic} style={styles.thumb}/>
-                    <View style={{flexDirection: 'column'}}>
-                        <Text style={{flex: 1, fontSize: 16, color: 'blue'}}>
-                            {item.name}
-                        </Text>
-                        <Text>
-                            {item.desc}
-                        </Text>
-                    </View>
-
-                </View>
-            </TouchableOpacity>
-        );
-    }
-
-    render() {
-        return (
-            <View style={{flex: 1}}>
-                <ListView dataSource={this.state.dataSource}
-                          renderRow={this.renderRow}/>
-            </View>
-        );
-    }
-}
+import DemoList from "../component/DemoList";
 
 class MainScreen extends Component {
 
@@ -93,14 +12,37 @@ class MainScreen extends Component {
         title: "测试覆盖"
     };
 
+    static propTypes = {
+        items: PropTypes.array.isRequired
+    };
+
     render() {
 
         // 将该页与其他页面路由建立起来
         const {navigate} = this.props.navigation;
 
+        const items = new Array([
+            {
+                name: '陶宏飞',
+                desc: '无力滔滔'
+            }, {
+                name: '周志忠',
+                desc: '123'
+            }, {
+                name: '舒韵',
+                desc: '3214'
+            }, {
+                name: '宋肖俊',
+                desc: '5234'
+            }, {
+                name: '杨念一',
+                desc: '3568'
+            }
+        ]);
+
         return (
             <ScrollView>
-                <List/>
+                <DemoList items={items}/>
                 <Button onPress={() => navigate('Profile')} title="跳转"/>
             </ScrollView>
         );
